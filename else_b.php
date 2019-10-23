1eb34531ab2c0cae0f1f85136d55f3796322819c
unknown <victorcabo9@gmail.com>
2019-10-23 15:48:55 +0200

    elseb.php

--- else_b.php
+++ else_b.php
@@ -1,11 +1,19 @@
 <?php
 function calcula($a){
-	if ($a >=5) {
-		echo "Apto";
-	}
-else {
-	echo "No apto";
-	}
-	}
-calcula (6);
+	$r="Notable";
+	if ($a < 7) 	
+	$r="Aprobado";
+	
+	if ($a < 5) 
+		$r="Pendiente";
+	
+	if ($a >= 7) 
+		$r="Notable";
+		
+	if ($a > 8.5) 
+		$r="Sobresaliente";
+		return $r;
+	}	
+	
+echo calcula (4.99);
 ?>
