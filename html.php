c577c3e26bde040f534fcbcf40386d24f2468d65
unknown <victorcabo9@gmail.com>
2019-11-06 16:50:17 +0100

    html.php

---
+++ html.php
@@ -0,0 +1,45 @@
+<html>
+<center>
+<table class="egt">
+			<table border=5px>
+			<tr>
+			<td>
+<center>
+<head> 
+<Title> 
+geografía, o algo
+</Title> 
+</head> 
+<h1> Hola :v</h1>
+<h3> Soy Sonic el erizo, y hoy me obligan a estar aquí </h3>
+
+<h6> (Así que por favor, atended, o llamad para que me saquen de aquí) </h6>
+<body>
+<body bgcolor=#CCFFFF>
+Hoy voy a mostrarte las comunidades autónomas bañadas en el <b> mar cantábrico. </b> <br> <p> <FONT SIZE=2> (Y más te vale no irte, que te arresto)</p>
+
+<img src= "Sonic.png" width=200 height=200>
+<table border=2px>
+			<tr>
+			<td> <b>Cantabria</b> </td>
+			<td> Santander </td>
+			</tr>
+
+			<tr>
+			<td> <b>País Vasco</b> </td>
+			<td> Vizcaya, Guipúzcoa </td>
+			</tr>
+			<tr>
+			<td> <b>Galicia</b> </td>
+			<td> Lugo </td>
+			</tr>
+			<tr>
+			<td> <b>Asturias</b> </td>
+			<td> Gijón, Áviles, oriente de Asturias </td>
+			</tr>
+</table>
+<p> <form>
+  <font size="2" color=#F781BE>  <b> Si queréis ver comunidades bañadas en otros mares, decidme</b></font> <u> <b><font size="2" color=#F781BE>aquí:</u> </b> <p> </font> <input type=" name="correo"><br> <button><p>Enviar</button> </p>
+</form>
+</center>
+</table>
